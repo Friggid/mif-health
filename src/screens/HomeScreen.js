5b11cb5b0a0541c377530da24e5dd5a61b83466a
Tautvydas <tmilciunas@gmail.com>
2017-10-23 14:58:24 +0300

    Initial push

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+import { MonoText } from '../components/StyledText';
+
+
+export default class HomeScreen extends React.Component {
+    static navigationOptions = {
+        header: null,
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Wadup mofos</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
